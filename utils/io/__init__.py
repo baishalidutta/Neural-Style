5e961195d8f3834ae53866eb1de148f468e56f65
Baishali Dutta <me@itsbaishali.com>
2022-04-03 08:35:19 +0200

    Added missing copyright info to __init__.py

--- utils/io/__init__.py
+++ utils/io/__init__.py
@@ -1,3 +1,8 @@
+__author__ = "Baishali Dutta"
+__copyright__ = "Copyright (C) 2021 Baishali Dutta"
+__license__ = "Apache License 2.0"
+__version__ = "0.1"
+
 # import the necessary packages
 from .hdf5datasetwriter import HDF5DatasetWriter
 from .hdf5datasetgenerator import HDF5DatasetGenerator
