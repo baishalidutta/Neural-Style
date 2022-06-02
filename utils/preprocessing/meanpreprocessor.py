f05fbee1c6251839e0dc81e2b4b0a69dc2246b45
Baishali Dutta <me@itsbaishali.com>
2022-06-02 08:26:06 +0200

    Fixed argument var names in mean preprocessor

--- utils/preprocessing/meanpreprocessor.py
+++ utils/preprocessing/meanpreprocessor.py
@@ -7,12 +7,12 @@ __version__ = "0.1"
 import cv2
 
 class MeanPreprocessor:
-	def __init__(self, rMean, gMean, bMean):
+	def __init__(self, r_mean, g_mean, b_mean):
 		# store the Red, Green, and Blue channel averages across a
 		# training set
-		self.rMean = rMean
-		self.gMean = gMean
-		self.bMean = bMean
+		self.r_mean = r_mean
+		self.g_mean = g_mean
+		self.b_mean = b_mean
 
 	def preprocess(self, image):
 		# split the image into its respective Red, Green, and Blue
@@ -20,9 +20,9 @@ class MeanPreprocessor:
 		(B, G, R) = cv2.split(image.astype("float32"))
 
 		# subtract the means for each channel
-		R -= self.rMean
-		G -= self.gMean
-		B -= self.bMean
+		R -= self.r_mean
+		G -= self.g_mean
+		B -= self.b_mean
 
 		# merge the channels back together and return the image
 		return cv2.merge([B, G, R])
