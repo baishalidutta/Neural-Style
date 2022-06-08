cb930a279bc75c5d2a24da297094245a2df2605e
Baishali Dutta <me@itsbaishali.com>
2022-06-08 10:39:22 +0200

    Fixed variable names in simple object detection

--- utils/utils/simple_obj_det.py
+++ utils/utils/simple_obj_det.py
@@ -7,55 +7,58 @@ __version__ = "0.1"
 from tensorflow.keras.applications import imagenet_utils
 import imutils
 
+
 def sliding_window(image, step, ws):
-	# slide a window across the image
-	for y in range(0, image.shape[0] - ws[1], step):
-		for x in range(0, image.shape[1] - ws[0], step):
-			# yield the current window
-			yield (x, y, image[y:y + ws[1], x:x + ws[0]])
+    # slide a window across the image
+    for y in range(0, image.shape[0] - ws[1], step):
+        for x in range(0, image.shape[1] - ws[0], step):
+            # yield the current window
+            yield (x, y, image[y:y + ws[1], x:x + ws[0]])
 
-def image_pyramid(image, scale=1.5, minSize=(224, 224)):
-	# yield the original image
-	yield image
 
-	# keep looping over the image pyramid
-	while True:
-		# compute the dimensions of the next image in the pyramid
-		w = int(image.shape[1] / scale)
-		image = imutils.resize(image, width=w)
+def image_pyramid(image, scale=1.5, min_size=(224, 224)):
+    # yield the original image
+    yield image
 
-		# if the resized image does not meet the supplied minimum
-		# size, then stop constructing the pyramid
-		if image.shape[0] < minSize[1] or image.shape[1] < minSize[0]:
-			break
+    # keep looping over the image pyramid
+    while True:
+        # compute the dimensions of the next image in the pyramid
+        w = int(image.shape[1] / scale)
+        image = imutils.resize(image, width=w)
 
-		# yield the next image in the pyramid
-		yield image
+        # if the resized image does not meet the supplied minimum
+        # size, then stop constructing the pyramid
+        if image.shape[0] < min_size[1] or image.shape[1] < min_size[0]:
+            break
 
-def classify_batch(model, batchROIs, batchLocs, labels, minProb=0.5,
-	top=10, dims=(224, 224)):
-	# pass our batch ROIs through our network and decode the
-	# predictions
-	preds = model.predict(batchROIs)
-	P = imagenet_utils.decode_predictions(preds, top=top)
+        # yield the next image in the pyramid
+        yield image
 
-	# loop over the decoded predictions
-	for i in range(0, len(P)):
-		for (_, label, prob) in P[i]:
-			# filter out weak detections by ensuring the
-			# predicted probability is greater than the minimum
-			# probability
-			if prob > minProb:
-				# grab the coordinates of the sliding window for
-				# the prediction and construct the bounding box
-				(pX, pY) = batchLocs[i]
-				box = (pX, pY, pX + dims[0], pY + dims[1])
 
-				# grab the list of predictions for the label and
-				# add the bounding box + probability to the list
-				L = labels.get(label, [])
-				L.append((box, prob))
-				labels[label] = L
+def classify_batch(model, batch_rois, batch_locs, labels, min_prob=0.5,
+                   top=10, dims=(224, 224)):
+    # pass our batch ROIs through our network and decode the
+    # predictions
+    preds = model.predict(batch_rois)
+    P = imagenet_utils.decode_predictions(preds, top=top)
 
-	# return the labels dictionary
-	return labels
+    # loop over the decoded predictions
+    for i in range(0, len(P)):
+        for (_, label, prob) in P[i]:
+            # filter out weak detections by ensuring the
+            # predicted probability is greater than the minimum
+            # probability
+            if prob > min_prob:
+                # grab the coordinates of the sliding window for
+                # the prediction and construct the bounding box
+                (pX, pY) = batch_locs[i]
+                box = (pX, pY, pX + dims[0], pY + dims[1])
+
+                # grab the list of predictions for the label and
+                # add the bounding box + probability to the list
+                L = labels.get(label, [])
+                L.append((box, prob))
+                labels[label] = L
+
+    # return the labels dictionary
+    return labels
