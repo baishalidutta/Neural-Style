f20b20882c9a105b7cece2a28af2ba0a59dd1af9
Baishali Dutta <me@itsbaishali.com>
2022-06-15 10:45:32 +0200

    Fixed formatting in callbacks package

--- utils/callbacks/epochcheckpoint.py
+++ utils/callbacks/epochcheckpoint.py
@@ -3,28 +3,30 @@ __copyright__ = "Copyright (C) 2021 Baishali Dutta"
 __license__ = "Apache License 2.0"
 __version__ = "0.1"
 
-# import the necessary packages
-from tensorflow.keras.callbacks import Callback
 import os
 
+# import the necessary packages
+from tensorflow.keras.callbacks import Callback
+
+
 class EpochCheckpoint(Callback):
-	def __init__(self, output_path, every=5, startAt=0):
-		# call the parent constructor
-		super(Callback, self).__init__()
+    def __init__(self, output_path, every=5, startAt=0):
+        # call the parent constructor
+        super(Callback, self).__init__()
 
-		# store the base output path for the model, the number of
-		# epochs that must pass before the model is serialized to
-		# disk and the current epoch value
-		self.output_path = output_path
-		self.every = every
-		self.intEpoch = startAt
+        # store the base output path for the model, the number of
+        # epochs that must pass before the model is serialized to
+        # disk and the current epoch value
+        self.output_path = output_path
+        self.every = every
+        self.intEpoch = startAt
 
-	def on_epoch_end(self, epoch, logs={}):
-		# check to see if the model should be serialized to disk
-		if (self.intEpoch + 1) % self.every == 0:
-			p = os.path.sep.join([self.outputPath,
-				"epoch_{}.hdf5".format(self.intEpoch + 1)])
-			self.model.save(p, overwrite=True)
+    def on_epoch_end(self, epoch, logs={}):
+        # check to see if the model should be serialized to disk
+        if (self.intEpoch + 1) % self.every == 0:
+            p = os.path.sep.join([self.outputPath,
+                                  "epoch_{}.hdf5".format(self.intEpoch + 1)])
+            self.model.save(p, overwrite=True)
 
-		# increment the internal epoch counter
-		self.intEpoch += 1
+        # increment the internal epoch counter
+        self.intEpoch += 1
