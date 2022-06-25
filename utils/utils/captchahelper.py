9ac1695c6c10b67280253ac0db1cd85bade5cb14
Baishali Dutta <me@itsbaishali.com>
2022-06-25 10:55:39 +0200

    Fixed copyright in utils package

--- utils/utils/captchahelper.py
+++ utils/utils/captchahelper.py
@@ -1,10 +1,9 @@
 __author__ = "Baishali Dutta"
-__copyright__ = "Copyright (C) 2021 Baishali Dutta"
+__copyright__ = "Copyright (C) 2021-2022 Baishali Dutta"
 __license__ = "Apache License 2.0"
 __version__ = "0.1"
 
 import cv2
-# import the necessary packages
 import imutils
 
 
