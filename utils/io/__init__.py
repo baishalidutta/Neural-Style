af600675bc446d1f888d0dd087e368d2cbd3e7b9
Baishali Dutta <me@itsbaishali.com>
2022-09-27 10:32:40 +0200

    Removed unnecessary import from io module

--- utils/io/__init__.py
+++ utils/io/__init__.py
@@ -2,7 +2,3 @@ __author__ = "Baishali Dutta"
 __copyright__ = "Copyright (C) 2021-2022 Baishali Dutta"
 __license__ = "Apache License 2.0"
 __version__ = "0.1"
-
-from .hdf5datasetgenerator import HDF5DatasetGenerator
-# import the necessary packages
-from .hdf5datasetwriter import HDF5DatasetWriter
