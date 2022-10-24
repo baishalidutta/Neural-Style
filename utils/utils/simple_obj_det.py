6f60786c35b6596538a9b70f5a081b37ed26f133
Baishali Dutta <me@itsbaishali.com>
2022-10-24 23:12:07 +0200

    Removed redundant paranthesis

--- utils/utils/simple_obj_det.py
+++ utils/utils/simple_obj_det.py
@@ -12,7 +12,7 @@ def sliding_window(image, step, ws):
     for y in range(0, image.shape[0] - ws[1], step):
         for x in range(0, image.shape[1] - ws[0], step):
             # yield the current window
-            yield (x, y, image[y:y + ws[1], x:x + ws[0]])
+            yield x, y, image[y:y + ws[1], x:x + ws[0]]
 
 
 def image_pyramid(image, scale=1.5, min_size=(224, 224)):
